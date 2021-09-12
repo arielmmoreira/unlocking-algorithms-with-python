9f79977db53d1f363e1627cb8215359a54795f95
arielmmoreira <arielmoraismoreira@gmail.com>
2021-09-11 23:01:41 -0300

    tests

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ import time, random
 
 
 def main():
-    arr = generateArray(1000)
+    arr = generateArray(10000)
 
-    selection(arr)
-    insertion(arr)
-    merge(arr)
-    quick(arr)
-    pythonSort(arr)
+    selectionSort = selection(arr)
+    insertionSort = insertion(arr)
+    mergeSort = merge(arr)
+    quickSort = quick(arr)
+    pythonSort = python(arr)
+    print("TOTAL TIME:")
+    print("Selection sort:", selectionSort)
+    print("Insertion sort:", insertionSort)
+    print("Merge sort:", mergeSort)
+    print("Quicksort:", quickSort)
+    print("Python built-in sort:", pythonSort)
 
     
 def generateArray(size):
@@ -22,7 +28,7 @@ def insertion(arr):
     sortArray.insertionSort()
     finalTime = time.time()
     totalTime = finalTime - startTime
-    print("Total time for insertion:", totalTime)
+    return totalTime
 
 def selection(arr):
     sortArray = Sort(arr)
@@ -30,7 +36,7 @@ def selection(arr):
     sortArray.selectionSort()
     finalTime = time.time()
     totalTime = finalTime - startTime
-    print("Total time for selection sort:", totalTime)
+    return totalTime
 
 def merge(arr):
     sortArray = Sort(arr)
@@ -38,7 +44,7 @@ def merge(arr):
     sortArray.mergeSort(0, len(arr) - 1)
     finalTime = time.time()
     totalTime = finalTime - startTime
-    print("Total time for merge sort:", totalTime)
+    return totalTime
 
 def quick(arr):
     sortArray = Sort(arr)
@@ -46,14 +52,13 @@ def quick(arr):
     sortArray.quickSort(0, len(arr) - 1)
     finalTime = time.time()
     totalTime = finalTime - startTime
-    print("Total time for quick sort:", totalTime)
+    return totalTime
 
-def pythonSort(arr):
-
+def python(arr):
     startTime = time.time()
     arr.sort()
     finalTime = time.time()
     totalTime = finalTime - startTime
-    print("Total time for python builti-in sort:", totalTime)
+    return totalTime
 
 main()
